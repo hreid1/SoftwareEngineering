eb50d419d43feea4615e52961a6691601e617817
Henry <henryreid05@gmail.com>
2023-11-07 16:41:43 +0000

    work

--- Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
+++ Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
@@ -5,13 +5,24 @@ using namespace std;
 
 int main()
 {
+	// opens the tables.txt document for writing
+	ofstream fn;
+	fn.open("tables.txt");
+	if (fn.is_open() == false)
+	{
+		cerr << " cannot create file " << endl;
+		return -1;
+	}
+	
 	for (unsigned int r = 2; r <= 12; r++) {
 		for (unsigned int c = 2; c <= 12; c++) {
-			cout << r * c << "\t";
+			fn << r * c << "\t";
 		}
-		cout << endl;
+		fn << endl;
 	}
-	cout << endl;
+	fn << endl;
+
+	fn.close();
 
 	return 0;
 }
